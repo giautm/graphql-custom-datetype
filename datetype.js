7a7020cde46ca954dfb95f51bacbe0d4754259f3
Giau Tran Minh <giau.tmg@gmail.com>
2017-02-05 11:51:49 +0700

    Update datetype.js

--- datetype.js
+++ datetype.js
@@ -1,37 +1,50 @@
-import { GraphQLScalarType } from 'graphql'
-import { GraphQLError } from 'graphql/error'
-import { Kind } from 'graphql/language'
+const {
+  GraphQLScalarType,
+} = require('graphql');
+const {
+  GraphQLError,
+} = require('graphql/error');
+const {
+  Kind,
+} = require('graphql/language');
 
-export default new GraphQLScalarType({
+const DateTime = new GraphQLScalarType({
   name: 'DateTime',
-  serialize (value) {
-    if (!(value instanceof Date)) {
-      // Is this how you raise a 'field error'?
-      throw new Error('Field error: value is not an instance of Date')
+  serialize: (value) => {
+    if (value instanceof Date) {
+      if (isNaN(value.getTime())) {
+        throw new Error('Field error: value is an invalid Date');
+      }
+
+      return value.toJSON();
     }
-    if (isNaN(value.getTime())) {
-      throw new Error('Field error: value is an invalid Date')
-    }
-    return value.toJSON()
+
+    throw new Error('Field error: value is not an instance of Date');
   },
-  parseValue (value) {
-    const date = new Date(value)
+  parseValue: (value) => {
+    const date = new Date(value);
     if (isNaN(date.getTime())) {
-      throw new Error('Field error: value is an invalid Date')
+      throw new Error('Field error: value is an invalid Date');
     }
-    return date
+
+    return date;
   },
-  parseLiteral (ast) {
-    if (ast.kind !== Kind.STRING) {
-      throw new GraphQLError('Query error: Can only parse strings to dates but got a: ' + ast.kind, [ast])
+  parseLiteral: (ast) => {
+    if (ast.kind !== Kind.STRING && ast.kind !== Kind.INT) {
+      throw new GraphQLError('Query error: Can only parse strings or integers to dates but got a: ' + ast.kind, [ast]);
     }
-    let result = new Date(ast.value)
+
+    let result = new Date(ast.value);
     if (isNaN(result.getTime())) {
-      throw new GraphQLError('Query error: Invalid date', [ast])
+      throw new GraphQLError('Query error: Invalid date', [ast]);
     }
-    if (ast.value !== result.toJSON()) {
-      throw new GraphQLError('Query error: Invalid date format, only accepts: YYYY-MM-DDTHH:MM:SS.SSSZ', [ast])
+    if (ast.kind === Kind.STRING && ast.value !== result.toJSON()) {
+      throw new GraphQLError('Query error: Invalid date format, only accepts: YYYY-MM-DDTHH:MM:SS.SSSZ', [ast]);
     }
-    return result
-  }
-})
+
+    return result;
+  },
+});
+
+exports["default"] = DateTime;
+module.exports = DateTime;
